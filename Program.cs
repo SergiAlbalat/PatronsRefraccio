c3ee8348b637cf35dfc011b8ed4c5e0e9b9b3ffb
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-01 13:29:02 +0100

    Funcionalitat 3 acabada Closes #5

--- Program.cs
+++ Program.cs
@@ -9,8 +9,10 @@ namespace PatronsRefraccio
             const string Msg1 = "Introdueix l'amplada del rectangle:";
             const string Msg2 = "Introdueix l'altura del rectangle:";
             const string Msg3 = "L'àrea del rectangle és: {0}";
+            const string Msg4 = "Introdueix el radi del cercle:";
+            const string Msg5 = "La circumferència del cercle és: {0}";
             const string Error = "El format del numero es incorrecte";
-            double width, height, area;
+            double width, height, area, radius, circumference;
 
             // Sol·licita l'entrada de l'usuari per calcular l'àrea d'un rectangle
             Console.WriteLine(Msg1);
@@ -31,11 +33,14 @@ namespace PatronsRefraccio
                 Console.WriteLine(Msg3, area);
 
                 // Sol·licita l'entrada de l'usuari per calcular la circumferència d'un cercle
-                Console.WriteLine("Introdueix el radi del cercle:");
-                double radius = Convert.ToDouble(Console.ReadLine());
-                double circumference = 2 * Math.PI * radius;
+                Console.WriteLine(Msg4);
+                do
+                {
+                    radius = Convert.ToDouble(Console.ReadLine());
+                } while (!MyMethods.IsNatural(radius));
+                circumference = MyMethods.CalculateCircumference(radius);
 
-                Console.WriteLine("La circumferència del cercle és: " + circumference);
+                Console.WriteLine(Msg5, circumference);
 
                 // Imprimeix un missatge basat en el valor de l'àrea
                 if (area > 50)
